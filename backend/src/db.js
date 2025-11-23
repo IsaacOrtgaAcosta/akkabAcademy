782cc27dd13c922ab2409a640d7f019d5ea22c8d
IsaacOrtga <ortga.isaac@gmail.com>
2025-11-23 01:35:00 +0100

    initial commit

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+module.exports = pool;
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/api/hello', (req, res,) => {
+    res.json({ message: 'Hola desde el backend en Docker modificado'})
+});
+
+app.get("/api/db-check", async (req, rers) => {
+    try {
+        const [rows] = await pool.query("SELECT NOW() AS now");
+        res.json({ok: true, time: rows[0].now});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ ok: false, error: "Error connecting to DB"});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuhando en el puerto ${PORT}`);
+});
