eae3c4a34d195aefda0b0a5b247a607a0c717f70
IsaacOrtgaAcosta <iortga.acosta@gmail.com>
2025-12-03 01:49:28 +0100

    feature: usePasswordToggle

--- frontend/src/app/features/auth/components/login-form/LoginForm.tsx
+++ frontend/src/app/features/auth/components/login-form/LoginForm.tsx
@@ -2,18 +2,35 @@ import { Button } from "@/app/shared/components/ui/button/Button";
 import styles from "./LoginForm.module.css";
 import { TextFieldComponent as TextField } from "@/app/shared/components/ui/text-field/TextField";
 import Logo from "@/assets/logo/logo-vertical.svg";
-
+import { ButtonVisibleIcon } from "@/app/shared/components/ui/buttonVisibleIcon/ButtonVisibleIcon";
+import usePasswordToggle from "@/app/shared/hooks/usePasswordToggle";
 
 export const LoginForm = () => {
+
+  const {type, visible, toggle} = usePasswordToggle();
+
+ 
+
   return (
-      <div className={styles.form}>
-        <img src={Logo} alt="logo"  className={styles.logo}/>
-        <h1>Acceder</h1>
-        <TextField label={'email'}></TextField>
-        <TextField label={'contraseña'}></TextField>
-        <Button variant={"success"} size={"md"} type={"submit"} className={styles.button}>
-          Entrar
-        </Button>
+    <div className={styles.form}>
+      <img src={Logo} alt="logo" className={styles.logo} />
+      <h1>Acceder</h1>
+      <TextField label={"email"}></TextField>
+      <div className={styles.inputWrapper}>
+        <TextField
+          label={"contraseña"}
+          type={type}
+        ></TextField>
+        <ButtonVisibleIcon isVisible={visible} passwordToggle={toggle} className={styles.buttonVisibleIcon}/>
       </div>
+      <Button
+        variant={"success"}
+        size={"md"}
+        type={"submit"}
+        className={styles.button}
+      >
+        Entrar
+      </Button>
+    </div>
   );
 };
--- frontend/src/app/shared/components/ui/text-field/TextField.tsx
+++ frontend/src/app/shared/components/ui/text-field/TextField.tsx
@@ -1,13 +1,12 @@
 import Box from '@mui/material/Box';
 import TextField, { type TextFieldProps as MuiTextFieldProps } from '@mui/material/TextField';
 
-export type OutlineTextFieldProps = MuiTextFieldProps & {
-    myProps?: string;
-};
+export type OutlineTextFieldProps = MuiTextFieldProps;
 
 export const TextFieldComponent: React.FC<OutlineTextFieldProps> = (
   {
     label,
+    ...rest
   }
 ) => {
   return (
@@ -17,7 +16,7 @@ export const TextFieldComponent: React.FC<OutlineTextFieldProps> = (
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label={label} variant="outlined"/>
+      <TextField id="outlined-basic" label={label} variant="outlined" {...rest}/>
     </Box>
   );
 };
